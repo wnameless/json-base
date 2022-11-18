4c1805304f50291bd74a8f8b5cad829c37a6a1e1
wmw <wnameless@gmail.com>
2022-11-18 19:52:56 +0800

    Open package to Gson

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,7 @@ module com.github.wnameless.json.base {
   requires static transitive com.fasterxml.jackson.databind;
   requires static transitive com.google.gson;
 
+  opens com.github.wnameless.json.base to com.google.gson;
+
   exports com.github.wnameless.json.base;
 }
